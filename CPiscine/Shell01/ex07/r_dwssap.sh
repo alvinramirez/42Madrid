1048c0ce1190d60edbbb14584c3ba2f9ae969719
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-07-08 23:43:11 +0200

    Added exercises from ex01 to ex07

---
+++ CPiscine/Shell01/ex07/r_dwssap.sh
@@ -0,0 +1,12 @@
+export FT_LINE1=7
+export FT_LINE2=15
+
+cat /etc/passwd | \
+grep -v '^#' | \
+awk 'NR % 2 == 0' | \
+cut -d ':' -f 1 | \
+rev | \
+#sort | \
+sed -n "${FT_LINE1},${FT_LINE2}p" | \
+paste -sd ", " - | \
+sed 's/$/./'
