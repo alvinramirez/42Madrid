47bc28b91c856cc76a3e4996c6ed34fda2d44355
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-07-18 03:04:13 +0200

    Modified exercise ex04 correctly as requested by the statement using a simple if

--- CPiscine/C00/ex04/ft_is_negative.c
+++ CPiscine/C00/ex04/ft_is_negative.c
@@ -1,13 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/05 01:29:24 by alvinram          #+#    #+#             */
+/*   Updated: 2024/07/16 15:57:39 by alvinram         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
+#include <stdio.h>
 
-void ft_is_negative(int n)
+void	ft_is_negative(int numero)
 {
-	write();
+	if (numero < 0)
+	{
+		write(1, &"N", 1);
+	}
+	else
+	{
+		write(1, &"P", 1);
+	}
 }
 
-int main()
+int	main(void)
 {
-	ft_is_negative();
-	return 0;
-}
+	int numero;
 
+	printf("Introduce un numero entero: ");
+	scanf("%d", &numero);
+	ft_is_negative(numero);
+	return (0);
+}
