4173c4ba9aea09f47382bcb6001f0720bcceede2
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-07-18 03:02:01 +0200

    Modified exercise ex03 correctly as requested by the statement using a while

--- CPiscine/C00/ex03/ft_print_numbers.c
+++ CPiscine/C00/ex03/ft_print_numbers.c
@@ -1,13 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/05 01:33:29 by alvinram          #+#    #+#             */
+/*   Updated: 2024/07/16 17:47:49 by alvinram         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_print_numbers(void)
+void	ft_print_numbers(void)
 {
-	write(0, "0123456789", 10);
+	char c;
+	c = '0';
+
+	while (c <= '9')
+	{
+		write(1, &c, 1);
+		c++;
+	}
+	
 }
 
-int main()
+int	main(void)
 {
 	ft_print_numbers();
-	return 0;
+	return (0);
 }
-
