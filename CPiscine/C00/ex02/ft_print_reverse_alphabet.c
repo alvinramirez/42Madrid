edf0452cb5bc1d8b1c38fd7975979c3e6b1f28ac
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-07-18 03:00:38 +0200

    Modified exercise ex02 correctly as requested by the statement printing the alphabet by using a while but changing the condition

--- CPiscine/C00/ex02/ft_print_reverse_alphabet.c
+++ CPiscine/C00/ex02/ft_print_reverse_alphabet.c
@@ -1,12 +1,32 @@
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/05 02:45:49 by alvinram          #+#    #+#             */
+/*   Updated: 2024/07/16 17:38:51 by alvinram         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void ft_print_reverse_alphabet(void)
+//#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
 {
-	write(1, "zyxwvutsrqponmlkjihgfedcba", 26);
+	char c;
+	c = 'z';
+	
+	while (c >= 'a')
+	{
+		write(1, &c, 1);
+		c--;
+	}
+	
 }
 
-int main()
+int	main(void)
 {
 	ft_print_reverse_alphabet();
-	return 0;
+	return (0);
 }
